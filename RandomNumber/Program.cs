ad4a7853ccb322807995b8862e4bab807b713ef4
Markus <markus.aus@tktk.ee>
2024-12-20 22:20:53 +0200

    create new app

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿Random rnd = new Random();
+
+int myRandomNum;
+
+int RandomSum = 0;
+
+for (int i = 0; i < 3   ; i++)
+{
+    myRandomNum = rnd.Next(0, 11);//0-10
+    RandomSum = RandomSum + myRandomNum;
+    Console.WriteLine($"My random number is:{myRandomNum}");
+}
+
+Console.WriteLine($"Random sum total: {RandomSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿// FOR - LOOP
+// DRY - DO NOT REPEAT YOURSELF
+
+int i = 0;
+
+for (int i = 0; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We don't need no education!");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have nice day!");
+
+//Console.WriteLine("We don't need no education!"); 
+//Console.WriteLine("We don't need no education!"); 
+//Console.WriteLine("We don't need no education!");
+//Console.WriteLine("We don't need no education!"); 
+
